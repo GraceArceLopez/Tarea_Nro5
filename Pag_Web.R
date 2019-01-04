d0b53da9fa93b3498ba04138863b19dd02739e21
GraceArceLopez <grace.arcel@utem.cl>
2019-01-04 15:38:02 -0200

    Actualización Tarea_Nro5, página web

---
+++ Pag_Web.R
@@ -0,0 +1,126 @@
+<html lang="es">
+  <head>
+  <style>
+  *{
+    margin:0px;
+    padding:0px;
+    
+  }	
+div#general{
+margin:auto;
+margin-top:30px;
+width:1100px;
+height:1100px;
+background-color:white;		
+}
+
+div#ofertas{
+
+float:left;
+width:650px;
+height:650px;
+background-color:white;	
+}
+
+div#novedades{
+
+float:right;
+width:350px;
+height:450px;
+background-color:white;	
+}
+
+
+
+
+</style>
+  <title></title>
+  
+  </head>
+  
+  <body> 
+  
+  <div id="general">
+    
+    <div id="novedades">
+      <table border="5px">
+        <tr><p class="violeta">
+          <th><p class="violeta">Producto</p></th>
+            <th><p class="violeta">Marca</p></th>
+              <th><p class="violeta">Precio</p></th>
+                <th><p class="violeta">Imagen de referencia</p></th>
+                  <th><p class="violeta">Supermercado</p></th>
+                    
+                    </tr>
+                    
+                    <tr>
+                    <th><p class="">Yoguth Batido</p></th>
+                      <th><p class="">Colun</p></th>
+                        <th><p class="">$220</p></th>
+                          <th><img src=https://images.lider.cl/wmtcl?set=imageSize[medium],imageURL[file:/productos/298197a.jpg],options[progressive]&call=url[file:catalog/sizing.chain]&sink=format[jpg],options[progressive] width"80px" height="80px"/></th>
+                            <th><p class="">Lider</p></th>
+                              </tr>
+                              
+                              <tr>
+                              <th><p class="">Arandanos</p></th>
+                                <th><p class="">-</p></th>
+                                  <th><p class="">$1.390</p></th>
+                                    <th><img src=https://images.lider.cl/wmtcl?set=imageSize[medium],imageURL[file:/productos/330908a.jpg],options[progressive]&call=url[file:catalog/sizing.chain]&sink=format[jpg],options[progressive] width="80px" height="80px"/></th>
+                                      <th><p class="">Lider</p></th>
+                                        </tr>
+                                        
+                                        <tr>
+                                        <th><p class="">Huevo mediano 30 un</p></th>
+                                          <th><p class="">Yemita</p></th>
+                                            <th><p class="">$3.490</p></th>
+                                              <th><img src=https://images.lider.cl/wmtcl?set=imageSize[medium],imageURL[file:/productos/319973a.jpg],options[progressive]&call=url[file:catalog/sizing.chain]&sink=format[jpg],options[progressive] width="80px" height="80px"/></th>
+                                                <th><p class="">Lider</p></th>
+                                                  </tr>
+                                                  
+                                                  <tr>
+                                                  <th><p class="">Salmon</p></th>
+                                                    <th><p class="">Lider</p></th>
+                                                      <th><p class="">$2.990</p></th>
+                                                        <th><img src=https://images.lider.cl/wmtcl?set=imageSize[medium],imageURL[file:/productos/587155a.jpg],options[progressive]&call=url[file:catalog/sizing.chain]&sink=format[jpg],options[progressive] width="80px" height="80px"/></th>
+                                                          <th><p class="">Lider</p></th>
+                                                            </tr>
+                                                            
+                                                            <tr>
+                                                            <th><p class="">Leche descremda</p></th>
+                                                              <th><p class="">Colun</p></th>
+                                                                <th><p class="">$790</p></th>
+                                                                  <th><img src=https://images.lider.cl/wmtcl?set=imageSize[medium],imageURL[file:/productos/5103a.jpg],options[progressive]&call=url[file:catalog/sizing.chain]&sink=format[jpg],options[progressive] width="80px" height="80px"/></th>
+                                                                    <th><p class="">Lider</p></th>
+                                                                      </tr>
+                                                                      
+                                                                      </table>
+                                                                      
+                                                                      </div>
+                                                                      
+                                                                      
+                                                                      <div id="ofertas">
+                                                                        <div><img src="https://do1pouckcwxot.cloudfront.net/mexico/uploads/2018/09/26093727/comidas-saludables.jpg" border="1px" alt="Alimentos" width="700px" height="250px"/></div> 
+                                                                          <div> <p align="justify"<h2>
+                                                                            <h1><br>Estos son los 5 alimentos más saludables </br></h1>
+                                                                            <h5><br><cite>Escuela de Salud Pública de la Universidad de Harvard, 2018</cite></br></h5>
+                                                                            
+                                                                            </h2>
+                                                                            <h4><br> <font face="calibri" color="navy">&#160 &#160 &#160
+                                                                              <p align="justify"<h2> El yogurth es, junto con los arándanos, las coles de Bruselas, el salmón y las nueces, uno de los alimentos más saludables, según un artículo de la profesora de nutrición de la Escuela de Salud Pública de la Universidad de Harvard, Teresa Fung, que señala que estos cinco alimentos deberían ser consumidos a diario para llevar una dieta sana.
+                                                                              
+                                                                              Según el texto de la profesora, se trata de alimentos que favorecen "nuestra salud porque aportan muchos nutrientes concentrados en pocas calorías y ayudan a mantener un correcto funcionamiento de nuestro organismo".
+                                                                              
+                                                                              En este sentido, el yogur, compuesto de leche y fermentos, destaca por su alto contenido en bacterias beneficiosas para la salud intestinal. Además de que es nutricionalmente denso ya que ofrece una gran cantidad de nutrientes en pocas calorías.
+                                                                              
+                                                                              Fung recomienda el consumo diario de yogur ya que "es una forma sencilla" de obtener probióticos, las bacterias beneficiosas para la salud que favorecen numerosos procesos de nuestro organismo. También destaca que "proporciona nutrientes importantes y beneficiosos como el calcio, las proteínas, el magnesio, la vitamina B12 y los ácidos grasos".</p>
+                                                                                
+                                                                                <br><br><p align="justify"<h2>&#160 &#160 &#160 El artículo también destaca las cualidades de las coles de Bruselas, con pocas calorías, alta densidad nutricional y altos en contenidos de vitaminas A, C y K, potasio y ácido fólico; el poder antioxidante de los arándanos; la fuente de ácidos grasos (Omega-3) y proteínas que supone el salmón, cuyo consumo se recomienda una vez a la semana; o las proteínas y vitamina E que aportan las nueces.
+                                                                                </br></br>
+                                                                                </br></h4></font></p>
+                                                                                </div>
+                                                                                </div>
+                                                                                
+                                                                                
+                                                                                </div>
+                                                                                </body>
+                                                                                </html>
